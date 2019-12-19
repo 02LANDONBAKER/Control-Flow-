6184b2d5b5d4c97ee7ab23e42cb10d776e25ca6f
Landon Baker <02LANDON.BAKER@gulllakecs.org>
2019-12-19 08:00:17 -0500

    Add GuessMyNumber While Loop

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,6 +1,22 @@
-'''
+"""
 Programmer: Landon Baker
 Date: 12.16.19
 Program: Guess My Number
-'''
+"""
 
+MyNumber = 7
+
+print("\nGuess a number between 1 & 10\n")
+
+# Ask users to guess.
+
+guess = int(input("Enter a Guess: "))
+
+# Keep asking users to guess my number until
+# it is equal to MyNumber.
+
+while guess != MyNumber:
+    print("\nNo, guess again: ")
+    guess = int(input("Enter a Guess: "))
+
+print("\nCongratulations on guessing my number")
