db5eabef5c38eaaf7e8e359d54f5c638c1a61725
Landon Baker <02LANDON.BAKER@gulllakecs.org>
2020-02-03 08:35:08 -0500

    Add Greeting

---
+++ ControlFlow.py
@@ -0,0 +1,16 @@
+# Date: 2.3.20
+# 
+
+name = input("\nWhat is your name?: ")
+
+
+# Create Functions Here
+"""
+def greeting():
+    print("Hi there " + name + "!")
+    print("Very nice to meet you " + name)
+"""
+
+# Call Functions Here
+
+# greeting()
