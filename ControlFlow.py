d3143554fbfe8e362a323dda5372461069d848d4
Landon Baker <02LANDON.BAKER@gulllakecs.org>
2020-02-04 14:43:18 -0500

    Add Outer For Loops With An Inner While Loop

--- ControlFlow.py
+++ ControlFlow.py
@@ -7,3 +7,21 @@ for i in range(3):
     print("Outer For Loop " + str(i))
     for k in range(4):
         print("\tInner For Loop " + str(k))
+
+
+
+print("\n ********************\n")
+
+"""
+Programmer: Landon Baker
+Date: 1.23.20
+Program: While Loop Nested Inside Of A For Loop
+"""
+
+for i in range(4):
+    print("For Loop: " + str(i))
+    x = i
+    while x >= 0:
+        print("\tWhile Loop: " + str(x))
+        x = x - 1
+        
