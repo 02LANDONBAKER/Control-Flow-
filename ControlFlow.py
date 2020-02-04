f8fe371dc199de10dde9d7a2d58338a5abed1e7d
Landon Baker <02LANDON.BAKER@gulllakecs.org>
2020-02-04 08:03:35 -0500

    Add Function & Parameter

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,5 +1,6 @@
+
 # Date: 2.3.20
-#
+# Programmer: Landon Baker
 
 # Declare Global Variables here
 
@@ -8,18 +9,25 @@ x = 15
 
 # Create Functions Here
 
+# Greeting Function
 """
 def greeting():
     print("Hi there " + name + "!")
     print("Very nice to meet you " + name)
 """
-
+# Functions & Local Variable
 def printSomething():
     x = 3
     print(x)
 
+# Functions & Parameters
+def printNumber(age): #Function name = printNumber with a PARAMETER of age.
+    print(age)
+
 # Call Functions Here
 
 # greeting()
-printSomething()
-print(x)
+# printSomething()
+# print(x)
+printNumber(28)
+printNumber(38)
